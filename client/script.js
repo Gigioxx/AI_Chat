240f5b5c1954e1412b3e6e5783aed0d5bc0d91e1
Gigiox <guillermo.casanova.b@gmail.com>
2023-02-06 00:55:08 -0300

    👽️ Using deployed api in vercel

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
 
   // Fetch data from server
 
-  const response = await fetch('https://ai-chat-server.vercel.app/', {
+  const response = await fetch('http://localhost:5555', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
